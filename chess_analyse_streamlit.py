23af55d09e43206419fd8f6fd1d2ca5fec09ee81
arthurmeltendorf <64423235+arthurmeltendorf@users.noreply.github.com>
2023-06-24 20:05:42 +0200

    Update chess_analyse_streamlit.py

--- chess_analyse_streamlit.py
+++ chess_analyse_streamlit.py
@@ -4,7 +4,6 @@ import csv
 import datetime
 import time
 import os  
-import matplotlib.pyplot as plt
 import streamlit as st
 from selenium import webdriver  
 from selenium.webdriver.common.keys import Keys  
